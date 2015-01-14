0b26eef8ab2fe4a9ce3be204e3fe9b6caca7fc36
John Carrick Smith <jcarricksmith@gmail.com>
2015-01-14 09:59:39 +0000

    Initiate variables in plotdata.cpp constructor.

--- src/plotdata.cpp
+++ src/plotdata.cpp
@@ -2,4 +2,18 @@
 
 PlotData::PlotData()
 {
+    xLabel = "";
+    yLabel = "";
+    xType = "";
+    yType = "";
+    xData.clear();
+    yData.clear();
+    trkName = "";
+    trkDate = "";
+    manScale = false;
+    xLo = 0.0;
+    xHi = 0.0;
+    yLo = 0.0;
+    yHi = 0.0;
+
 }
